bec65a350bcd4f286c19e502da565d252a67093b
BuildTools <adam.bensalah@yahoo.fr>
2017-09-10 21:49:46 +0100

    Added some comments

--- lol_ping_plot.py
+++ lol_ping_plot.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 servers = {"NA":"104.160.131.3","EUW":"104.160.141.3","EUNE":"104.160.142.3","OCE":"104.160.156.1","LAN":"104.160.136.3"}
 
+## Get server name as command line argument
+
 if len(sys.argv) != 2:
 	print('Usage: py lol_ping_plot.py [server] - plot server ping')
 	sys.exit()
@@ -11,6 +13,8 @@ if len(sys.argv) != 2:
 server = sys.argv[1].upper()
 
 cmd = "ping "+servers[server]+" -t"
+
+## Start probing for latency
 
 proc = Popen(cmd, shell=True, bufsize=1, stdout=PIPE, universal_newlines = True)
 
@@ -29,17 +33,14 @@ for line in iter(proc.stdout.readline,''):
 
 	ping = res_regexp.search(line)
 	ms = 0
-	if(ping):
+	if(ping): # Got ping correctly
 		ms = int(ping.group(1))
 		x_values.append(request_num)
 		ping_values.append(ms)
-	elif(line.find("D") != -1):
+	else: # Can't get response / packet loss
 		ms = 999
 		x_values.append(request_num)
 		ping_values.append(ms)
-	else:
-		print(line[0:5])
-	print("ping : "+str(ms))
 
 	## Show the data
 	plt.plot(x_values,ping_values)
