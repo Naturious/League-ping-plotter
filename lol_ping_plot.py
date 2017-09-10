75b219edd4184c3ce1c141e1b355f90a2754d3d2
BuildTools <adam.bensalah@yahoo.fr>
2017-09-10 21:46:44 +0100

    Added command line args and all servers support

--- lol_ping_plot.py
+++ lol_ping_plot.py
@@ -1,21 +1,29 @@
 import sys,re
 from subprocess import Popen, PIPE
-
 import matplotlib.pyplot as plt
 
-cmd = "ping riot.de -t"
+servers = {"NA":"104.160.131.3","EUW":"104.160.141.3","EUNE":"104.160.142.3","OCE":"104.160.156.1","LAN":"104.160.136.3"}
+
+if len(sys.argv) != 2:
+	print('Usage: py lol_ping_plot.py [server] - plot server ping')
+	sys.exit()
+
+server = sys.argv[1].upper()
+
+cmd = "ping "+servers[server]+" -t"
 
 proc = Popen(cmd, shell=True, bufsize=1, stdout=PIPE, universal_newlines = True)
 
 res_regexp = re.compile(r'=(\d+) ms')
 
-i = 0
+
+request_num = 0
 x_values = []
 ping_values = []
 
 
 for line in iter(proc.stdout.readline,''):
-	i += 1
+	request_num += 1
 
 	## Get ping from server
 
@@ -23,15 +31,15 @@ for line in iter(proc.stdout.readline,''):
 	ms = 0
 	if(ping):
 		ms = int(ping.group(1))
-		x_values.append(i)
+		x_values.append(request_num)
 		ping_values.append(ms)
 	elif(line.find("D") != -1):
 		ms = 999
-		x_values.append(i)
+		x_values.append(request_num)
 		ping_values.append(ms)
 	else:
 		print(line[0:5])
-	print("ms : "+str(ms))
+	print("ping : "+str(ms))
 
 	## Show the data
 	plt.plot(x_values,ping_values)
