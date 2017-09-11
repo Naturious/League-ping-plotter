95da1c39b2ca52947ccdcec6d6e97b5b9441079a
BuildTools <adam.bensalah@yahoo.fr>
2017-09-11 21:31:12 +0100

    added support for custom servers

--- lol_ping_plot.py
+++ lol_ping_plot.py
@@ -4,15 +4,27 @@ import matplotlib.pyplot as plt
 
 servers = {"NA":"104.160.131.3","EUW":"104.160.141.3","EUNE":"104.160.142.3","OCE":"104.160.156.1","LAN":"104.160.136.3"}
 
+def print_help():
+	print('Usage: py lol_ping_plot.py [server]/[custom [server_ip]] - plot server ping')
+	print('Supported servers: EUW, NA, EUNE, OCE, LAN')
 ## Get server name as command line argument
 
-if len(sys.argv) != 2:
-	print('Usage: py lol_ping_plot.py [server] - plot server ping')
+if len(sys.argv) < 2 or len(sys.argv) > 3:
+	print_help()
 	sys.exit()
 
-server = sys.argv[1].upper()
+server_ip = ""
 
-cmd = "ping "+servers[server]+" -t"
+if(sys.argv[1].upper() == "CUSTOM"):
+	if(len(sys.argv) != 3):
+		print_help()
+		sys.exit()
+	server_ip = sys.argv[2]
+else:
+	server_name = sys.argv[1].upper()
+	server_ip = servers[server_name]
+
+cmd = "ping "+server_ip+" -t"
 
 ## Start probing for latency
 
